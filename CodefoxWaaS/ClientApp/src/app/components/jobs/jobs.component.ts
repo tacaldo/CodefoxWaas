b3d892fd057b383a0465b1c462273f53b771bfbc
codefoxtechnologies <tacaldo@gmail.com>
2020-08-05 17:41:16 -0400

    Add jobs mock stuff.

--- CodefoxWaaS/ClientApp/src/app/components/jobs/jobs.component.ts
+++ CodefoxWaaS/ClientApp/src/app/components/jobs/jobs.component.ts
@@ -1,14 +1,34 @@
-﻿import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Job } from '../../classes/job';
+import { JobService } from '../../services/job.service';
+import { INITIAL_CONFIG } from '@angular/platform-server';
+
 
 @Component({
     selector: 'app-jobs',
     templateUrl: './jobs.component.html',
     styleUrls: ['./jobs.component.css']
 })
-/** jobs component*/
-export class JobsComponent {
-    /** jobs ctor */
-    constructor() {
 
-    }
+/** jobs component*/
+
+
+
+export class JobsComponent implements OnInit {
+
+
+  selectedJob: Job;
+
+  jobs: Job[];
+
+
+
+    /** jobs ctor */
+  constructor(private jobService: JobService) { }
+
+  ngOnInit(): void {
+        throw new Error("Method not implemented.");
+  }
+
+
 }
---
+++ CodefoxWaaS/ClientApp/src/app/mock-data/mock-jobs.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.JOBS = [
+    { id: 11, name: 'Construction Day Labor' },
+    { id: 12, name: 'Warehouse Worker' },
+    { id: 13, name: 'Furniture Mover' },
+    { id: 14, name: 'Load Freight Trucks' },
+    { id: 15, name: 'Cleanup Crew - Festival' },
+    { id: 16, name: 'Welder' },
+    { id: 17, name: 'Mechanic Assistant' },
+    { id: 18, name: 'Roofing Helper' }
+];
+//# sourceMappingURL=mock-jobs.js.map
